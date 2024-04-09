13a65959bcf6eea294fb971e5401fed3d568536b
one1erp <avigaile@one1.co.il>
2024-04-09 09:03:43 +0300

    Initial commit with additional files

---
+++ ExternalProgramCreateFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Configuration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        // Check if the required parameter is provided
+        if (args.Length != 1)
+        {
+            return;
+        }
+
+        string cuurrenCoaId = args[0];
+
+        string url = ConfigurationManager.AppSettings["URL"] + cuurrenCoaId;
+
+        // Create the request
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "POST";
+        request.ContentLength = 0;
+
+        // Make the request and read the response
+        string apiReqResult;
+        using (WebResponse response = request.GetResponse())
+        {
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    apiReqResult = reader.ReadToEnd();
+                }
+            }
+        }
+
+    }
+}
